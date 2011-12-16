02c4f1d76964facfccf7f6337cba2bf5ab4b7b1d
foudfou <foudil.newbie+git@gmail.com>
2011-12-16 18:08:18 +0100

    cleaning

--- src/modules/commons.js
+++ src/modules/commons.js
@@ -1,6 +1,6 @@
 /* -*- Mode: js2; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
-/* for now, logging facilities (imported from logging.jsm are automatically
+/* for now, logging facilities (imported from logging.jsm) are automatically
    provided by this module */
 var EXPORTED_SYMBOLS =
   [ "firetray", "LOG", "WARN", "ERROR", "FIREFOX_ID", "THUNDERBIRD_ID",
@@ -15,12 +15,12 @@ const Cu = Components.utils;
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://firetray/logging.jsm");
 
-const FIREFOX_ID = "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}";
+const FIREFOX_ID     = "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}";
 const THUNDERBIRD_ID = "{3550f703-e582-4d05-9a08-453d09bdfdc6}";
-const SONGBIRD_ID = "songbird@songbirdnest.com";
-const SUNBIRD_ID = "{718e30fb-e89b-41dd-9da7-e25a45638b28}";
-const SEAMONKEY_ID = "{92650c4d-4b8e-4d2a-b7eb-24ecf4f6b63a}";
-const CHATZILLA_ID = "{59c81df5-4b7a-477b-912d-4e0fdf64e5f2}";
+const SONGBIRD_ID    = "songbird@songbirdnest.com";
+const SUNBIRD_ID     = "{718e30fb-e89b-41dd-9da7-e25a45638b28}";
+const SEAMONKEY_ID   = "{92650c4d-4b8e-4d2a-b7eb-24ecf4f6b63a}";
+const CHATZILLA_ID   = "{59c81df5-4b7a-477b-912d-4e0fdf64e5f2}";
 
 const FT_NOTIFICATION_DISABLED = 0;
 const FT_NOTIFICATION_UNREAD_MESSAGE_COUNT = 1;
