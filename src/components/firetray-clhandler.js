13f9b31ed933785ea54ef497e25f5f394483827c
foudfou <foudil.newbie+git@gmail.com>
2012-03-30 14:39:16 +0200

    experimental command-line handling
    will be useful to have a command for toggling windows that can be linked to the
    wm keyboard shortcut mapping.
    
    TODO: find out why helpInfo not working

---
+++ src/components/firetray-clhandler.js
@@ -0,0 +1,40 @@
+// https://developer.mozilla.org/en/Chrome/Command_Line
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://firetray/logging.jsm");
+
+function firetayCommandLineHandler() {}
+firetayCommandLineHandler.prototype = {
+  classDescription: "firetayCommandLineHandler",
+  classID: Components.ID('{a9c9cc52-4d6c-45c2-a73f-0be1bd60aaa6}'),
+  contractID: "@mozilla.org/commandlinehandler/general-startup;1?type=firetray",
+  _xpcom_categories: [{
+    category: "command-line-handler",
+    entry: "m-firetray"
+  }],
+
+  QueryInterface: XPCOMUtils.generateQI([
+    Ci.nsICommandLineHandler
+  ]),
+
+  /* nsICommandLineHandler */
+  handle: function clh_handle(cmdLine)
+  {
+    if (cmdLine.handleFlag("firetray", false)) {
+      firetray.WARN("*** CALL ***");
+      cmdLine.preventDefault = true;
+    }
+  },
+
+  // NOTE: change the help info as appropriate, but follow the guidelines in
+  // nsICommandLineHandler.idl specifically, flag descriptions should start at
+  // character 24, and lines should be wrapped at 72 characters with embedded
+  // newlines, and finally, the string should end with a newline
+  helpInfo: "  -firetray            FOUDIL WAS HERE\n"
+};
+
+var NSGetFactory = XPCOMUtils.generateNSGetFactory([firetayCommandLineHandler]);
