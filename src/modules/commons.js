foudfou/FireTray

commit 9afa777c656daf9f042df6977c038f4e632b3e48

Reshape icon directories, and fix Makefile.

src/Makefile
@@ -92,14 +92,13 @@ chrome_sources := $(chrome_sources_js)								\
                $(wildcard $(chrome_source_root)/content/*.xul)					\
                $(wildcard $(chrome_source_root)/content/*.xml)					\
                $(wildcard $(chrome_source_root)/content/*.css)					\
-               $(wildcard $(chrome_source_root)/skin/*.css)					\
-               $(wildcard $(chrome_source_root)/skin/*.gif)					\
-               $(wildcard $(chrome_source_root)/skin/*.png)					\
-               $(wildcard $(chrome_source_root)/skin/*.svg)					\
-               $(wildcard $(chrome_source_root)/skin/linux/icons/hicolor/22x22/apps/*.png)	\
-               $(wildcard $(chrome_source_root)/skin/linux/icons/gnome/22x22/status/*.png)	\
-               $(wildcard $(chrome_source_root)/skin/winnt/*.bmp)				\
-               $(wildcard $(chrome_source_root)/skin/winnt/*.ico)				\
+               $(wildcard $(chrome_source_root)/skin/icons/*.css)				\
+               $(wildcard $(chrome_source_root)/skin/icons/*.gif)				\
+               $(wildcard $(chrome_source_root)/skin/icons/*.png)				\
+               $(wildcard $(chrome_source_root)/skin/icons/*.svg)				\
+               $(wildcard $(chrome_source_root)/skin/icons/linux/hicolor/22x22/*/*.png)		\
+               $(wildcard $(chrome_source_root)/skin/icons/winnt/*.bmp)				\
+               $(wildcard $(chrome_source_root)/skin/icons/winnt/*.ico)				\
                $(wildcard $(chrome_source_root)/locale/*/*.dtd)					\
                $(wildcard $(chrome_source_root)/locale/*/*.properties)
 

src/chrome/content/options.xul
@@ -18,7 +18,7 @@
   </broadcasterset>
 
   <prefpane id="pref-pane-windows" label="&windows_options;"
-            image="chrome://firetray/skin/pref-windows.svg">
+            image="chrome://firetray/skin/icons/pref-windows.svg">
 
     <preferences>
       <preference id="pref_hides_on_close" name="extensions.firetray.hides_on_close" type="bool"/>
@@ -63,7 +63,7 @@
   </prefpane>
 
   <prefpane id="pref-pane-icon" label="&icon_options;"
-            image="chrome://firetray/skin/pref-icon.svg">
+            image="chrome://firetray/skin/icons/pref-icon.svg">
 
     <preferences>
       <preference id="pref_app_icon_type" name="extensions.firetray.app_icon_type" type="int"/>
@@ -125,7 +125,7 @@
   </prefpane>
 
   <prefpane id="pref-pane-mail" label="&mail_options;"
-            image="chrome://firetray/skin/pref-mail.svg">
+            image="chrome://firetray/skin/icons/pref-mail.svg">
 
     <preferences>
       <preference id="pref_mail_notification_enabled" name="extensions.firetray.mail_notification_enabled" type="bool" />
@@ -315,7 +315,7 @@
   </prefpane>
 
   <prefpane id="pref-pane-chat" label="&chat_options;"
-            image="chrome://firetray/skin/pref-chat.svg">
+            image="chrome://firetray/skin/icons/pref-chat.svg">
 
     <preferences>
       <preference id="pref_chat_icon_enable" name="extensions.firetray.chat_icon_enable" type="bool" />

commit 972f1c642c01c8b6ef8c960428175433c7960463

Fix check marks invisible in option 'Included accounts'.
Bug introduced in 9afa777c65.

src/Makefile
@@ -92,7 +92,7 @@ chrome_sources := $(chrome_sources_js)								\
                $(wildcard $(chrome_source_root)/content/*.xul)					\
                $(wildcard $(chrome_source_root)/content/*.xml)					\
                $(wildcard $(chrome_source_root)/content/*.css)					\
-               $(wildcard $(chrome_source_root)/skin/icons/*.css)				\
+               $(wildcard $(chrome_source_root)/skin/*.css)					\
                $(wildcard $(chrome_source_root)/skin/icons/*.gif)				\
                $(wildcard $(chrome_source_root)/skin/icons/*.png)				\
                $(wildcard $(chrome_source_root)/skin/icons/*.svg)				\

src/modules/commons.js
@@ -217,17 +217,7 @@ firetray.Utils = {
     return protocolHandler.getFileFromURLSpec(aPath).path;
   },
 
-  dumpObj: function(obj) {
-    let str = "";
-    for(let prop in firetray.js.listAllProperties(obj)) {
-      try {
-        str += "obj["+prop+"]: " + obj[prop] + "\n";
-      } catch(e) {
-        str += "obj["+prop+"]: Unavailable\n";
-      }
-    }
-    log.info(str);
-  },
+  dumpObj: function(obj) {},    // Use JSON.stringify(obj) instead.
 
   _nsResolver: function(prefix) {
     var ns = {